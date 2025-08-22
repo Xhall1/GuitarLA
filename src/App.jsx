7af0972554c3741ef16478b092e245173e408cd9
Xhall1 <gridoracones@gmail.com>
2025-08-22 07:16:31 +0200

    feat: adding props and Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import {Header} from './components/Header';
+import { useState } from 'react';
+import { Guitar } from './components/Guitar';
+import { Header } from './components/Header';
+import { db } from './data/db';
 
 function App() {
+
+  const [data, setData] = useState(db);
 
   return (
     <>
       <Header />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Our collection</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+            />
+          )
+          )}
+
+        </div>
+      </main>
     </>
   )
 }
