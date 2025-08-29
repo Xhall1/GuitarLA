ed508ab6cfdd9e9352224fb6cdfe771ee039e1f8
Xhall1 <gridoracones@gmail.com>
2025-08-29 15:20:17 -0500

    refactor: removing bad src

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export const Header = ({ cart, removeFromCart, increaseQuantity, decreaseQuantit
             <a href="index.html">
               <img
                 className="img-fluid"
-                src="./public/img/logo.svg"
+                src="/img/logo.svg"
                 alt="imagen logo"
               />
             </a>
@@ -24,7 +24,7 @@ export const Header = ({ cart, removeFromCart, increaseQuantity, decreaseQuantit
             <div className="carrito">
               <img
                 className="img-fluid"
-                src="./public/img/carrito.png"
+                src="img/carrito.png"
                 alt="imagen carrito"
               />
 
